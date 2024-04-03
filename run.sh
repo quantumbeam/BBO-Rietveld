7586bbdec987408f0edad781fc80dc5fe9da10c2
resnant <resnant@outlook.jp>
2024-04-03 12:51:15 +0000

    update script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-docker run --rm -v ${SCRIPT_DIR}/:/bbo_rietveld -p 8888:8888 -it resnant/bbo-rietveld:v1.0
+docker run --rm -v ${SCRIPT_DIR}/:/workspace -p 8888:8888 -it resnant/bbo-rietveld:v1.1 jupyter lab --allow-root --ip 0.0.0.0
